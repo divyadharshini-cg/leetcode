a0c1040f53d8945847f67d2ad5414ee7538f7c30
divyadharshini-cg <divyacg2006@gmail.com>
2026-08-10 12:07:19 +0530

    Time: 4 ms (57.14%) | Memory: 12.3 MB (64.29%) - LeetSync

---
+++ 3484-lexicographically-smallest-string-after-a-swap/lexicographically-smallest-string-after-a-swap.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def getSmallestString(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        s=list(s)
+        for i in range(len(s)-1):
+            if (int(s[i])%2)==(int(s[i+1])%2):
+                if s[i]>s[i+1]:
+                    s[i],s[i+1]=s[i+1],s[i]
+                    break
+        return "".join(s)
